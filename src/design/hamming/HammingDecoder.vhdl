c42b6e3ef1ce83d8d92cf00ed48589fde99d46ad
atlihrafnarnason <atli@gormur.com>
2022-09-28 11:32:40 +0200

    Implemented HammingEncoder and started implementation on HammingDecoder

--- src/design/hamming/HammingDecoder.vhdl
+++ src/design/hamming/HammingDecoder.vhdl
@@ -10,6 +10,7 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity HammingDecoder is
     port(
@@ -21,17 +22,37 @@ entity HammingDecoder is
 end entity;
 
 architecture Behavioral of HammingDecoder is 
-
-    -- define necessary signals here
-
+    signal error_pos: std_logic_vector(2 downto 0);
+    signal hamming_parity_bits: std_logic_vector(2 downto 0);
+    signal data_bits: std_logic_vector(3 downto 0);
+    signal parity_ok: std_logic;
 begin
+    -- first see if parity is correct, since parity is calculated
+    -- on the code word. If parity is hi, there is no error.
+    
+    -- make it easier to work with data and parity bits
+    data_bits <= x_encoded(6 downto 4) & x_encoded(2);
+    hamming_parity_bits <= x_encoded(3) & x_encoded(1 downto 0);
+    
+    -- check parity of coded message (ignore the linter, this is legal in vhdl-2008)
+    parity_ok <= x_encoded(7) and (xor x_encoded); 
 
-    -- describe combinational logic here
+    error_pos(0) <= (hamming_parity_bits(0) xor data_bits(0)) xor (data_bits(1) xor data_bits(3));
+    error_pos(1) <= (hamming_parity_bits(1) xor data_bits(0)) xor (data_bits(2) xor data_bits(3));
+    error_pos(2) <= (hamming_parity_bits(2) xor data_bits(1)) xor (data_bits(2) xor data_bits(3));
+    
+    -- so when error_pos != 0, there is an error in the message. if the parity is good AND there is an error
+    -- we have 2 errors. 
 
-    -- dummy connections
-    x <= x_encoded(6 downto 4) & x_encoded(2);
-    corrected <= '0';
-    error <= '0';
-    -- dummy connections
+    x <= data_bits;
+
+    error <= '1' when (?? (parity_ok)) and ((error_pos) /= x"0") else '0';
+
+    corrected <= '1' when ((error_pos) /= x"0") else '0';
 
 end architecture;
+
+
+
+
+
--- src/design/hamming/HammingEncoder.vhdl
+++ src/design/hamming/HammingEncoder.vhdl
@@ -20,14 +20,22 @@ end entity;
 
 architecture Behavioral of HammingEncoder is
 
-    -- define necessary signals here
+    signal hamming_parity: std_logic_vector(2 downto 0);
+    signal even_parity: std_logic;
 
 begin
 
     -- describe combinational logic here
+    hamming_parity(0) <= ('0' xor x(0)) xor (x(1) xor x(3));
+    hamming_parity(1) <= ('0' xor x(0)) xor (x(2) xor x(3));
+    hamming_parity(2) <= ('0' xor x(1)) xor (x(2) xor x(3));
+    
+    -- unary xor is available in vhdl-2008 and & is the concatenation operator, not AND. 
+    even_parity <= (xor (hamming_parity & x));
+    
+    x_encoded(7 downto 4) <= even_parity & x(3 downto 1);
+    x_encoded(3 downto 0) <= hamming_parity(2) & x(0) & hamming_parity(1 downto 0);
 
-    -- dummy connections
-    x_encoded <= x"00";
-    -- dummy connections
+
 
 end architecture;
