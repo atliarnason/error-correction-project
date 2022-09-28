a710158dc371aaa186eec92ced39071954fc1211
atlihrafnarnason <atli@gormur.com>
2022-09-28 11:31:58 +0200

    Added exhaustive testing for the HammingEncoder

--- src/test/HammingEncoderTb.vhdl
+++ src/test/HammingEncoderTb.vhdl
@@ -30,9 +30,35 @@ begin
 
     dut: HammingEncoder port map(x => dut_in, x_encoded => dut_out);
 
+    -- Even parity, so changed parity bit in expected result to 0 for x'0';
     process begin
-        check(x"0", "00000001", dut_in, dut_out, expected);
-        
+        check(x"0", "00000000", dut_in, dut_out, expected);
+        check(x"1", "10000111", dut_in, dut_out, expected);        
+        check(x"2", "10011001", dut_in, dut_out, expected);        
+        check(x"3", "00011110", dut_in, dut_out, expected);        
+        check(x"4", "10101010", dut_in, dut_out, expected);        
+        check(x"5", "00101101", dut_in, dut_out, expected);        
+        check(x"6", "00110011", dut_in, dut_out, expected);        
+        check(x"7", "10110100", dut_in, dut_out, expected);        
+        check(x"8", "01001011", dut_in, dut_out, expected);        
+        check(x"9", "11001100", dut_in, dut_out, expected);        
+        check(x"a", "11010010", dut_in, dut_out, expected);        
+        check(x"b", "01010101", dut_in, dut_out, expected);        
+        check(x"c", "11100001", dut_in, dut_out, expected);        
+        check(x"d", "01100110", dut_in, dut_out, expected);        
+        check(x"e", "01111000", dut_in, dut_out, expected);        
+        check(x"f", "11111111", dut_in, dut_out, expected);        
+
+
+
+
+
+
+
+
+
+
+
         -- add other stimuli here
 
         std.env.stop(0);
